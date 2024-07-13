214397a2f2c8efab76eb05a226217987e151e6af
yeonjy <yeonjypaper@gmail.com>
2024-07-13 22:29:36 +0900

    feat: init

---
+++ src/main/java/com/example/devcrew/DevcrewApplication.java
@@ -0,0 +1,13 @@
+package com.example.devcrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevcrewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevcrewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devcrew/DevcrewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devcrew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevcrewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
