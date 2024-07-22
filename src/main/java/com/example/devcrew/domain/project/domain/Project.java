d9a5e86a94d043717a6a95820d24b54938a171e1
sseongeun <96720963+sseongeun@users.noreply.github.com>
2024-07-22 23:55:56 +0900

    feat(#4) : Project 도메인 & ProjectTag 구현

---
+++ src/main/java/com/example/devcrew/domain/project/domain/Project.java
@@ -0,0 +1,29 @@
+package com.example.devcrew.domain.project.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private ProjectTag projectTag;
+
+    private String summary;
+
+    private String teamName;
+
+    private String role;
+
+    private String period;
+
+}
