6782be5409c8c9fd850323a1cec85e1354a0a28d
yeonjy <yeonjypaper@gmail.com>
2024-07-22 22:13:38 +0900

    feat(#1): OAuth 관련 설정 구현

---
+++ src/main/java/com/example/devcrew/global/oauth2/CustomOAuth2User.java
@@ -0,0 +1,23 @@
+package com.example.devcrew.global.oauth2;
+
+import com.example.devcrew.domain.member.entity.Role;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
+@Getter
+public class CustomOAuth2User extends DefaultOAuth2User {
+    private final String email;
+    private final Role role;
+
+    public CustomOAuth2User(Collection<? extends GrantedAuthority> authorities,
+                            Map<String, Object> attributes, String nameAttributeKey,
+                            String email, Role role) {
+        super(authorities, attributes, nameAttributeKey);
+        this.email = email;
+        this.role = role;
+    }
+}
