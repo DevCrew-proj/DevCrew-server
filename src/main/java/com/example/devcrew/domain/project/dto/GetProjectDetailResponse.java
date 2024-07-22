c0cb5090fd242c99a6c2fe7a12a1aafebf56cdb1
sseongeun <96720963+sseongeun@users.noreply.github.com>
2024-07-23 00:45:45 +0900

    feat(#4) : 포트폴리오에서 프로젝트 하나씩 반환하는 dto

---
+++ src/main/java/com/example/devcrew/domain/project/dto/GetProjectDetailResponse.java
@@ -0,0 +1,41 @@
+package com.example.devcrew.domain.project.dto;
+
+import com.example.devcrew.domain.project.domain.Project;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class GetProjectDetailResponse {
+    private Long id;
+
+    private String name;
+
+    private String tag;
+
+    private String imageUrl;
+
+    private String summary;
+
+    private String teamName;
+
+    private String role;
+
+    private String period;
+
+    public static GetProjectDetailResponse from (Project project){
+        return GetProjectDetailResponse.builder()
+                .id(project.getId())
+                .name(project.getName())
+                .tag(project.getProjectTag().getTag())
+                .imageUrl(project.getImageUrl())
+                .summary(project.getSummary())
+                .teamName(project.getTeamName())
+                .role(project.getRole())
+                .period(project.getPeriod())
+                .build();
+    }
+}
