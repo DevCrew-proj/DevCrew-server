1503801b85ab38f801f004d0477bc740d0db35f6
Jangwon0319 <ryujw0319@gmail.com>
2024-07-22 20:41:40 +0900

    feat(#6): Feedback 도메인 구현

---
+++ src/main/java/com/example/devcrew/domain/feedback/domain/enums/FeedbackTag.java
@@ -0,0 +1,5 @@
+package com.example.devcrew.domain.feedback.domain.enums;
+
+public enum FeedbackTag {
+    PLAN, DESIGN, FRONTEND, BACKEND, CODE, OTHER
+}
---
+++ src/main/java/com/example/devcrew/domain/feedback/domain/enums/Language.java
@@ -0,0 +1,5 @@
+package com.example.devcrew.domain.feedback.domain.enums;
+
+public enum Language {
+    JAVA, JAVASCRIPT, KOTLIN, PYTHON, SWIFT, C, OTHER
+}
