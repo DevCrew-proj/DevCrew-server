a0be1b8885c42fa3f661d2aece458f8ba9067ee7
mcmcjangn <jmc@catholic.ac.kr>
2024-07-24 04:07:43 +0900

    feat(#8): Team, TeamMatching entity 구현

---
+++ src/main/java/com/example/devcrew/domain/team/entity/Os.java
@@ -0,0 +1,7 @@
+package com.example.devcrew.domain.team.entity;
+
+public enum Os {
+    OS,
+    AOS
+
+}
---
+++ src/main/java/com/example/devcrew/domain/team/entity/Team.java
@@ -0,0 +1,64 @@
+package com.example.devcrew.domain.team.entity;
+
+import com.example.devcrew.global.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Team")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Team extends BaseTimeEntity {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+/* **** Contest, NormalMember FK ****
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "contest_id", nullable = false)
+    private Contest contestId;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "normal_member_id", nullable = false)
+    private NormalMember normalMemberId;
+
+*/
+
+    @OneToMany(mappedBy = "team", fetch = FetchType.LAZY)
+    private List<TeamMatching> teamMatchingId;
+
+
+    @Column(length = 30)
+    private String name;
+
+    @Column(length = 20)
+    private String password;
+
+    private Integer peopleNum;
+
+    @Column(length = 30)
+    private String serviceName;
+
+    @Column(length = 255)
+    private String planUrl;
+
+    @Column(length = 30)
+    private String formDevelop;
+
+    @Column(length = 50)
+    private String equipment;
+
+    @Enumerated(EnumType.STRING)
+    private Os os;
+
+
+
+}
