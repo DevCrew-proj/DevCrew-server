34a3fe7a4f887fb1fe9ad82ec5a06d8be2e3c89e
yeonjy <yeonjypaper@gmail.com>
2024-07-23 23:24:11 +0900

    feat(#13): @Enumerated 설정

--- src/main/java/com/example/devcrew/domain/member/entity/CompanyMember.java
+++ src/main/java/com/example/devcrew/domain/member/entity/CompanyMember.java
@@ -1,14 +1,17 @@
 package com.example.devcrew.domain.member.entity;
 
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.Getter;
 
 @Embeddable
+@Getter
 public class CompanyMember {
     private String companyName;
     private String businessRegistrationNumber;
     private String ceoName;
 
-
+    @Enumerated(EnumType.STRING)
     private CompanySize companySize;
-
 }
