77fdfe36aa5ce9a95af2cae6eb3d15998e8e2f3b
Seongeun Cho <96720963+sseongeun@users.noreply.github.com>
2024-07-26 02:55:17 +0900

    bug: redis 관련 cicd문제로 어노테이션 추가

--- src/main/java/com/example/devcrew/DevcrewApplication.java
+++ src/main/java/com/example/devcrew/DevcrewApplication.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableRedisRepositories
 public class DevcrewApplication {
 
     public static void main(String[] args) {
