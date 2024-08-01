73145afc389a8c11b56a9fe945c041b0c006771b
yeonjy <yeonjypaper@gmail.com>
2024-08-01 23:19:28 +0900

    feat/#23: CompanyMember Builder 구현

--- src/main/java/com/example/devcrew/domain/member/entity/CompanyMember.java
+++ src/main/java/com/example/devcrew/domain/member/entity/CompanyMember.java
@@ -3,15 +3,31 @@ package com.example.devcrew.domain.member.entity;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import lombok.Getter;
+import lombok.*;
 
-@Embeddable
 @Getter
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CompanyMember {
+    private String contactNumber;
+    private String responsiblePartyName;
+
     private String companyName;
     private String businessRegistrationNumber;
     private String ceoName;
 
     @Enumerated(EnumType.STRING)
     private CompanySize companySize;
+
+    @Builder
+    public CompanyMember(String contactNumber, String responsiblePartyName, String companyName,
+                         String businessRegistrationNumber, String ceoName, CompanySize companySize) {
+        this.contactNumber = contactNumber;
+        this.responsiblePartyName = responsiblePartyName;
+        this.companyName = companyName;
+        this.businessRegistrationNumber = businessRegistrationNumber;
+        this.ceoName = ceoName;
+        this.companySize = companySize;
+    }
+
 }
