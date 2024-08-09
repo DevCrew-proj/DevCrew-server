b3e2abb6344a4d752e0fc6d7c60639ef40311b2b
mcmcjangn <jmc@catholic.ac.kr>
2024-08-09 15:34:46 +0900

    feat(#46) : 공모전 정보 조회 기능 추가

--- src/main/java/com/example/devcrew/domain/team/api/TeamController.java
+++ src/main/java/com/example/devcrew/domain/team/api/TeamController.java
@@ -1,5 +1,7 @@
 package com.example.devcrew.domain.team.api;
 
+import com.example.devcrew.domain.contest.dto.response.GetContestDetailResponseDTO;
+import com.example.devcrew.domain.contest.service.ContestQueryService;
 import com.example.devcrew.domain.team.dto.request.CreateTeamRequestDTO;
 import com.example.devcrew.domain.team.entity.Team;
 import com.example.devcrew.domain.team.service.TeamService;
@@ -7,8 +9,11 @@ import com.example.devcrew.domain.team.dto.request.ApplyTeamRequestDTO;
 import com.example.devcrew.domain.team.dto.response.GetMemberInfoResponseDTO;
 import com.example.devcrew.domain.team.entity.TeamMatching;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +23,7 @@ import org.springframework.web.bind.annotation.*;
 public class TeamController {
 
     private final TeamService teamService;
+    private final ContestQueryService contestQueryService;
 
     @GetMapping("/create")
     @Operation(summary = "팀 구성 기능에서 멤버 정보 조회", description = "팀 생성 시 해당 멤버의 이름과 전화번호 반환(자동완성기능).")
@@ -26,14 +32,14 @@ public class TeamController {
     }
 
     @PostMapping("/create")
-    @Operation(summary = "Create a new team")
+    @Operation(summary = "팀 구성하기")
     public Team createTeamsByContestAndMember(@RequestBody @Valid CreateTeamRequestDTO createTeamRequestDTO) {
         return teamService.createTeamsByContestAndMember(createTeamRequestDTO);
     }
 
     @GetMapping("/apply")
-    @Operation(summary = "팀 신청 기능에서 멤버 정보 조회", description = "팀원 신청서 작성 시 해당 멤버의 이름과 전화번호를 반환합니다.(자동완성기능)")
-    public GetMemberInfoResponseDTO getMemberInfo() {
+    @Operation(summary = "팀 신청 기능에서 멤버 정보 조회", description = "팀원 신청서 작성 시 해당 멤버의 이름과 전화번호를 반환(자동완성기능)")
+    public GetMemberInfoResponseDTO getMemberInfoForApply() {
         return teamService.GetMemberInfo();
     }
 
@@ -41,5 +47,24 @@ public class TeamController {
     @Operation(summary = "팀 신청하기")
     public TeamMatching applyTeam(@RequestBody @Valid ApplyTeamRequestDTO applyTeamRequestDTO) {
         return teamService.applyToTeam(applyTeamRequestDTO);
+    }
+
+
+    @GetMapping("/apply/{contestId}")
+    @Operation(summary = "공모전 상세 조회", description =  "팀 신청 페이지 상단에 표시할 공모전 상세 정보 조회")
+    @Parameters({
+            @Parameter(name = "contestId", description = "공모전의 아이디, PathVariable 입니다."),
+    })
+    public GetContestDetailResponseDTO getContestDetailForApply(@PathVariable Long contestId) {
+        return contestQueryService.findContestDetailById(contestId);
+    }
+
+    @GetMapping("/create/{contestId}")
+    @Operation(summary = "공모전 상세 조회", description = "팀 구성 페이지 상단에 표시할 공모전 상세 정보 조회")
+    @Parameters({
+            @Parameter(name = "contestId", description = "공모전의 아이디, PathVariable 입니다."),
+    })
+    public GetContestDetailResponseDTO getContestDetailForCreate(@PathVariable Long contestId) {
+        return contestQueryService.findContestDetailById(contestId);
     }
 }
