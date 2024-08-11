b05cd64ac88236a353edb8474666a3ae980dce91
Jangwon0319 <ryujw0319@gmail.com>
2024-08-11 16:19:20 +0900

    feat(#56): WebConfig 구현

---
+++ src/main/java/com/example/devcrew/global/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.example.devcrew.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://13.124.194.211")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
