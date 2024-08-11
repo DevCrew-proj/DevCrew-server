c1418046f9f71b3fd78de30edfac0ccd5052fa44
Jangwon0319 <ryujw0319@gmail.com>
2024-08-11 17:11:41 +0900

    refactor(#58) : webconfig 수정

--- src/main/java/com/example/devcrew/global/config/WebConfig.java
+++ src/main/java/com/example/devcrew/global/config/WebConfig.java
@@ -10,8 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("http://13.124.194.211")
+                .allowedOrigins("http://localhost:3000", "http://13.124.194.211")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
