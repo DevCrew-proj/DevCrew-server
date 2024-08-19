065729ecaac9b2691b21ec9716545a3fef2c86a2
yeonjy <yeonjypaper@gmail.com>
2024-08-20 02:15:36 +0900

    feat(#97): 기업 종류 추가

--- src/main/java/com/example/devcrew/domain/member/entity/CompanySize.java
+++ src/main/java/com/example/devcrew/domain/member/entity/CompanySize.java
@@ -8,7 +8,13 @@ import lombok.RequiredArgsConstructor;
 public enum CompanySize {
     CONGLOMERATE("대기업"),
     SMALL_AND_MID_SIZE_COMAPNY("중소기업"),
-    PUBLIC_ENTERPRISE("공공기업"),
+    PUBLIC_ENTERPRISE("공공기업/공기업"),
+    FOREIGN_ENTERPRISE("외국계 기업"),
+    NON_PROFIT_ORGANIZATION("비영리 단체"),
+    START_UP("비영리 단체"),
+    FINANCE("금융권"),
+    SCHOOL("학교"),
+    ETC("기타")
     ;
 
     private final String name;
