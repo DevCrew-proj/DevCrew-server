28365c249dc7c32b399e3225d96846d9fc89910a
yeonjy <81320703+yeonjy@users.noreply.github.com>
2024-08-20 16:34:38 +0900

    refactor(#99): setter 삭제

--- src/main/java/com/example/devcrew/domain/member/dto/request/PostMemberProfileRequest.java
+++ src/main/java/com/example/devcrew/domain/member/dto/request/PostMemberProfileRequest.java
@@ -6,10 +6,8 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostMemberProfileRequest {
 
